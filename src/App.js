2b2bf3468504dbe9b7fe6847dc99ef37f822eb54
ryhhill1998 <ryhhill1998@outlook.com>
2022-10-27 10:17:06 +0100

    Added basic html and css to authentication forms

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">Authentication App</h1>
+      <div className="forms-container">
+        <form className="form form--signup">
+          <h2>Sign up form</h2>
+          <label className="form-label">
+            Email
+            <input className="form-input" type="text" name="name" />
+          </label>
+          <label className="form-label">
+            Password
+            <input className="form-input" type="text" name="name" />
+          </label>
+          <label className="form-label">
+            Confirm password
+            <input className="form-input" type="text" name="name" />
+          </label>
+          <input className="submit-btn" type="submit" value="Submit" />
+        </form>
+
+        <form className="form form--signin">
+          <h2>Sign in form</h2>
+          <label className="form-label">
+            Email
+            <input className="form-input" type="text" name="name" />
+          </label>
+          <label className="form-label">
+            Password
+            <input className="form-input" type="text" name="name" />
+          </label>
+          <input className="submit-btn" type="submit" value="Submit" />
+        </form>
+      </div>
     </div>
   );
 }
