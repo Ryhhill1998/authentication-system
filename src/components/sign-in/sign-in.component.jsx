b69b2c719d53b44d4dd54fb2b02493e120fe5859
ryhhill1998 <ryhhill1998@outlook.com>
2022-10-27 18:00:30 +0100

    Added form input change and form submit functionality

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -11,16 +11,48 @@ const SignIn = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { email, password } = formFields;
 
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormFields((prev) => {
+      const updatedFields = { ...prev };
+      updatedFields[name] = value;
+      return updatedFields;
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formFields);
+    resetFormFields();
+  };
+
   return (
-    <form className="form form--signin">
+    <form className="form form--signin" onSubmit={handleSubmit}>
       <h2>Sign in form</h2>
       <label className="form-label">
         Email
-        <input className="form-input" type="text" name="name" />
+        <input
+          className="form-input"
+          type="text"
+          name="email"
+          value={email}
+          onChange={handleChange}
+        />
       </label>
       <label className="form-label">
         Password
-        <input className="form-input" type="text" name="name" />
+        <input
+          className="form-input"
+          type="text"
+          name="password"
+          value={password}
+          onChange={handleChange}
+        />
       </label>
       <input className="submit-btn" type="submit" value="Sign in" />
     </form>
--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -12,20 +12,58 @@ const SignUp = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { email, password, confirmPassword } = formFields;
 
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormFields((prev) => {
+      const updatedFields = { ...prev };
+      updatedFields[name] = value;
+      return updatedFields;
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formFields);
+    resetFormFields();
+  };
+
   return (
-    <form className="form form--signup">
+    <form className="form form--signup" onSubmit={handleSubmit}>
       <h2>Sign up form</h2>
       <label className="form-label">
         Email
-        <input className="form-input" type="text" name="name" />
+        <input
+          className="form-input"
+          type="text"
+          name="email"
+          value={email}
+          onChange={handleChange}
+        />
       </label>
       <label className="form-label">
         Password
-        <input className="form-input" type="text" name="name" />
+        <input
+          className="form-input"
+          type="text"
+          name="password"
+          value={password}
+          onChange={handleChange}
+        />
       </label>
       <label className="form-label">
         Confirm password
-        <input className="form-input" type="text" name="name" />
+        <input
+          className="form-input"
+          type="text"
+          name="confirmPassword"
+          value={confirmPassword}
+          onChange={handleChange}
+        />
       </label>
       <input className="submit-btn" type="submit" value="Sign up" />
     </form>
